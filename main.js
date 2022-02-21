853c3cacede9044e22210fd910c71b74f247db70
l0rdcafe <ismailarafa18@gmail.com>
2022-02-21 14:37:00 -0500

    refactor a bit

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // html elements
   const canvas = document.querySelector("#canvas");
   const headerInput = document.querySelector("#header");
   const footerInput = document.querySelector("#footer");
   const fileInput = document.querySelector("#file");
   const saveBtn = document.querySelector("#save");
+  // canvas 2d context
   const ctx = canvas.getContext("2d");
 
+  // page state
   let imgFile = null;
-  let headerText = "";
-  let footerText = "";
+  let headerText = headerInput.value ?? "";
+  let footerText = footerInput.value ?? "";
 
+  // draw dank meme text
   const drawText = () => {
     ctx.font = "36pt Impact";
     ctx.textAlign = "center";
@@ -23,45 +27,59 @@ document.addEventListener("DOMContentLoaded", () => {
     ctx.strokeText(footerText, canvas.width / 2, canvas.height - 20);
   };
 
-  const drawMeme = (img) => {
-    if (img == null) {
+  // main drawing logic
+  const drawMeme = () => {
+    if (imgFile == null) {
       ctx.strokeRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(defaultImg, 1, 1, canvas.width, canvas.height);
     } else {
-      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+      ctx.drawImage(imgFile, 0, 0, canvas.width, canvas.height);
     }
 
     drawText();
   };
 
+  // placeholder img used on page load
+  const defaultImg = new Image();
+  defaultImg.src =
+    "https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png?w=640";
+  // draw meme when img loads
+  defaultImg.onload = () => {
+    // first draw
+    drawMeme();
+  };
+
+  // event listeners for input fields
   headerInput.addEventListener("keyup", ({ target: { value } }) => {
     headerText = value;
-    drawMeme(imgFile);
+    drawMeme();
   });
 
   footerInput.addEventListener("keyup", ({ target: { value } }) => {
     footerText = value;
-    drawMeme(imgFile);
+    drawMeme();
   });
 
+  // img file handler
   fileInput.addEventListener(
     "change",
     (e) => {
       const [file] = e.target.files;
-      console.log({ file });
       const reader = new FileReader();
       reader.onload = ({ target: { result } }) => {
         const img = new Image();
-        img.onload = () => {
-          drawMeme(img);
-        };
         img.src = result;
         imgFile = img;
+        img.onload = () => {
+          drawMeme();
+        };
       };
       reader.readAsDataURL(file);
     },
     false
   );
 
+  // opens given img in a new tab
   saveBtn.addEventListener(
     "click",
     () => {
@@ -69,6 +87,4 @@ document.addEventListener("DOMContentLoaded", () => {
     },
     false
   );
-
-  drawMeme(imgFile, headerText, footerText);
 });
